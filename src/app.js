e6f4b5a8e8126bd434dcbd7d82b73b5c41760bb4
Digory <digory1@googlemail.com>
2018-09-11 16:42:58 +0100

    Added display rendering to instrument_info_view

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const SelectView = require('./views/select_view.js');
+const MusicalInstruments = require('./models/musical_instruments.js');
+const InstrumentInfoView = require('./views/instrument_info_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const selectElement = document.querySelector('#instrument-families');
   const selectView = new SelectView(selectElement);
   selectView.bindEvents();
+
+  const musicalInstruments = new MusicalInstruments();
+  musicalInstruments.bindEvents();
+  
+  const instrumentDisplayElement = document.querySelector('#display-info');
+  const instrumentInfoView = new InstrumentInfoView(instrumentDisplayElement);
+  instrumentInfoView.bindEvents();
 });
---
+++ src/views/instrument_info_view.js
@@ -0,0 +1,32 @@
+const PubSub = require('./helpers/pub_sub.js');
+
+const InstrumentInfoView = function(container){
+    this.container = container;
+};
+
+InstrumentInfoView.prototype.bindEvents = function(){
+    PubSub.subscribe('MusicalInstruments:clicked-instrument-ready', (event)=>{
+        const instrumentFamilyToShow = event.detail;
+        this.display(instrumentFamilyToShow);
+    });
+};
+
+InstrumentInfoView.prototype.display = function(instrumentFamilyToShow){
+    const headingElement = document.createElement('h2');
+    const paragraphElement = document.createElement('p');
+    const smallHeadingElement = document.createElement('h3');
+    const unorderedListElement = document.createElement('ul');
+    this.container.innerHTML = "";
+    headingElement.textContent = instrumentFamilyToShow.name;
+    paragraphElement.textContent = instrumentFamilyToShow.description;
+    smallHeadingElement.textContent = "Instruments include:";
+    for(let instrument of instrumentFamilyToShow){
+        const listElement = document.createElement('li');
+        listElement.textContent = instrument;
+        unorderedListElement.appendChild(listElement);
+    };
+    this.container.appendChild(headingElement);
+    this.container.appendChild(paragraphElement);
+    this.container.appendChild(smallHeadingElement);
+    this.container.appendChild(unorderedListElement);
+};
